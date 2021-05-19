39d08363fa2a440648789c2582be77935ed23986
idCasper <61905668+idCasper@users.noreply.github.com>
2021-05-19 12:48:01 +0700

    add feed content layout

---
+++ app/src/main/java/id/codes/al_kindi_app/FeedContentActivity.java
@@ -0,0 +1,14 @@
+package id.codes.al_kindi_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FeedContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_content);
+    }
+}
